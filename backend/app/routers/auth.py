4bed85eef100c0d5ed2204697476d7eba68eccde
Dipanshu <dipanshu@ce.iitr.ac.in.com>
2026-07-07 14:24:20 +0530

    Implement JWT login authentication

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 from app.database.dependencies import get_db
 from app.schemas.user import UserCreate, UserResponse
 from app.services.auth_service import register_user
+from app.schemas.user import UserLogin, Token
+from app.services.auth_service import login_user
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -17,3 +19,14 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
 
     return new_user
+
+
+@router.post("/login", response_model=Token)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    token = login_user(db, user)
+
+    if token is None:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    return token
--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate
 from app.core.security import hash_password
+from app.core.security import verify_password, create_access_token
+from app.schemas.user import UserLogin
 
 
 def register_user(db: Session, user: UserCreate):
@@ -24,3 +26,20 @@ def register_user(db: Session, user: UserCreate):
     db.refresh(new_user)
 
     return new_user
+
+
+def login_user(db: Session, user: UserLogin):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        return None
+
+    if not verify_password(user.password, db_user.hashed_password):
+        return None
+
+    access_token = create_access_token(
+        data={"sub": db_user.email, "role": db_user.role}
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
